be4455db3aec38684025bfff2705b2f7f2ce3121
leto <leto@fondation.org>
2024-04-15 11:48:55 +0200

    adding devShells

--- flake.nix
+++ flake.nix
@@ -60,8 +60,8 @@
 	let
 		inherit (self) outputs;
 		lib = nixpkgs.lib // home-manager.lib;
-    systems = [
-      "x86_64linux"
+        systems = [
+      "x86_64-linux"
       "aarch64-linux"
     ];
     forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
