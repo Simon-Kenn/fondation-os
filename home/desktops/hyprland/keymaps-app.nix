bec6aa2615ea8128ef39fcf2982d8f550f32a822
leto <leto@fondation.org>
2024-04-20 10:31:31 +0200

    save signal directories

--- home/desktops/hyprland/keymaps-app.nix
+++ home/desktops/hyprland/keymaps-app.nix
@@ -1,13 +1,21 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   wayland.windowManager.hyprland.settings = {
     bind = let
       grimblast = lib.getExe pkgs.grimblast;
+      terminal = config.home.sessionVariables.TERMINAL;
+
+      defaultApp = type: "${lib.getExe' pkgs.gtk3 "gtk-launch"} $(${lib.getExe' pkgs.xdg-utils "xdg-mime"} query default ${type})";
+      browser = defaultApp "x-scheme-handler/https";
     in [
-      "SUPER,Return,exec, alacritty"
+      # Program bindings
+      "SUPER,Return,exec, ${terminal}"
+      "SUPER,b,exec, ${browser}"
+
       "SUPER,space,exec,wofi -S drun"
       "SUPER,h,exec,wofi -S run"
       "SUPER,l,exec,swaylock --grace 0 --fade-in 0"
--- home/desktops/programs/signal-dekstop.nix
+++ home/desktops/programs/signal-dekstop.nix
@@ -2,4 +2,8 @@
   home.packages = with pkgs; [
     signal-desktop
   ];
+
+  home.persistence."/persist/home/leto".directories = [
+    ".config/Signal"
+  ];
 }
