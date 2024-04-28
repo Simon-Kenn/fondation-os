fd7f5bb833671dfcd61ac7d19acfc03af9b04ed4
leto <leto@fondation.org>
2024-04-28 16:11:38 +0200

    update zellij

--- home/core/cli/multiplexers/zellij/default.nix
+++ home/core/cli/multiplexers/zellij/default.nix
@@ -6,5 +6,13 @@
 
   xdg.configFile = {
     "zellij/config.kdl".source = ./config.kdl;
+    "zellij/layouts/mine.kdl".text = ''
+      layout {
+      	pane size=1 borderless=true {
+      		plugin location="zellij:compact-bar"
+      	}
+      	pane
+      }
+    '';
   };
 }
