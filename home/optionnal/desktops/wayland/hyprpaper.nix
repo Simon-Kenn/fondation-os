49434124e7bfa5b4ad2929aee06c707f02415c44
leto <leto@fondation.org>
2024-09-03 14:41:48 +0200

    update hyprpaper with config.wallpape

--- home/optionnal/desktops/wayland/hyprpaper.nix
+++ home/optionnal/desktops/wayland/hyprpaper.nix
@@ -1,15 +1,11 @@
-{ inputs, pkgs, ...}:{
+{ inputs, pkgs, config, ...}:{
   services.hyprpaper = {
     enable = true;
-
     package = inputs.hyprpaper.packages.${pkgs.system}.default;
 
-
     settings = {
-      preloads = [
-        "~/fondation/modules/home/desktops/wayland/hyprpaper/girl_and_cat.jpg"
-      ];
-      wallpapers = [", ./girl_and_cat.jpg"];
+      preloads = ["${config.wallpaper}"];
+      wallpapers = [", ${config.wallpaper}"];
     };
   };
 }
