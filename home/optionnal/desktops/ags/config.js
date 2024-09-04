262c7ab265aded7bbde050aa13b56f336ecb57ae
leto <leto@fondation.org>
2024-09-04 12:06:36 +0200

    commiting something wrong, sorry!

--- home/optionnal/desktops/ags/config.js
+++ home/optionnal/desktops/ags/config.js
@@ -1,19 +1,14 @@
-function Bar(monitor = 0) {
-	const myLabel =  Wigdet.Label({
-		label: 'some exemple content',
-	})
+const entry = App.configDir + '/ts/main.ts'
+const outdir = '/tmp/ags/js'
 
-	return Widget.Window({
-		monitor,
-		name: `bar${monitor}`,
-		anchor: ['top', 'left', 'right'],
-		child: myLabel,
-	})
+try {
+    await Utils.execAsync([
+        'bun', 'build', entry,
+        '--outdir', outdir,
+        '--external', 'resource://*',
+        '--external', 'gi://*',
+    ])
+    await import(`file://${outdir}/main.js`)
+} catch (error) {
+    console.error(error)
 }
-
-App.config({ 
-	windows: [
-		Bar(0),
-		Bar(1),
-	],
-})
