3f3c0debce7c7ec6fc9010df42c891f8f825d210
leto <leto@fondation.org>
2024-09-04 15:03:02 +0200

    rewrite hyprland bindings

--- home/optionnal/desktops/hyprland/binds.nix
+++ home/optionnal/desktops/hyprland/binds.nix
@@ -118,5 +118,72 @@ in {
       "$mod,mouse:273,resizewindow"
     ];
 
+    bind = let  
+      grimblast = lib.getExe pkgs.grimblast;
+      #pactl = lib.getExe' pkgs.tesseract;
+      notify-send = lib.getExe' pkgs.libnotify "notify-send";
+
+      terminal = config.home.sessionVariables.TERMINAL;
+      defaultApp = type: "${lib.getExe' pkgs.gtk3 "gtk-launch"} $(${lib.getExe' pkgs.xdg-utils "xdg-mime"} query default ${type})";
+      browser = defaultApp "x-scheme-handler/https";
+
+    in[
+      # compositor command
+      "$mod SHIFT, c, killactive"
+      "$mod SHIFT, Q, exit"
+
+      "$mod, j, togglesplit"
+      "$mod, f, fullscreen, 1"
+      "$mod SHIFT, f, fullscreen, 0"
+      "$mod SHIFT, space, togglefloating"
+
+      "$mod, é, splitratio,-0.25"
+      "$mod SHIFT, é, splitratio, -0.3333333"
+
+      "$mod, è, splitratio, 0.25"
+      "$mod SHIFT, è, splitratio, 0.3333333"
+
+      "$mod, g, togglegroup"
+      "$mod, l, lockactivegroup, toggle"
+      "$mod, tab, changegroupactive, f"
+      "$mod SHIFT, tab, changegroupactive, b"
+
+      "$mod, apostrophe, workspace, previous"
+
+      "$mod, u, togglespecialworkspace"
+      "$mod SHIFT, u, movetoworkspacesilent, special"
+      "$mod, i, pseudo"
+
+      # programs
+
+    ]
+    ++
+    # Change workspace
+    (map (n: "$mod, ${n.key}, workspace, name:${n.name}") workspaces)
+    ++
+    # Move window to workspace
+    (map (n: "$mod SHIFT, ${n.key}, movetoworkspacesilent, name:${n.name}") workspaces)
+    ++
+    # Move focus
+    (lib.mapAttrsToList (key: direction: "$mod, ${key}, movefocus, ${direction}") directions)
+    ++
+    # Swap windows
+    (lib.mapAttrsToList (key: direction: "$mod SHIFT, ${key}, swapwindow, ${direction}") directions)
+    ++
+    # Move windows
+    (lib.mapAttrsToList (
+        key: direction: "$mod CONTROL, ${key}, movewindoworgroup, ${direction}"
+      )
+      directions)
+    ++
+    # Move monitor focus
+    (lib.mapAttrsToList (key: direction: "$mod ALT, ${key}, focusmonitor, ${direction}") directions)
+    ++
+    # Move workspace to other monitor
+    (lib.mapAttrsToList (
+        key: direction: "$mod ALTSHIFT,${key}, movecurrentworkspacetomonitor,${direction}"
+      )
+      directions);
+
   };
 }
