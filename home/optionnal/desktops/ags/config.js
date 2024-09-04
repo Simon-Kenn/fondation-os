a5e4bfaefc78690b2c89fde77a674bb6b8f72615
leto <leto@fondation.org>
2024-09-04 07:56:35 +0200

    system update, basic ags config

--- home/optionnal/desktops/ags/config.js
+++ home/optionnal/desktops/ags/config.js
@@ -1,5 +1,19 @@
-App.config({
-	windows: [
+function Bar(monitor = 0) {
+	const myLabel =  Wigdet.Label({
+		label: 'some exemple content',
+	})
 
-	]
+	return Widget.Window({
+		monitor,
+		name: `bar${monitor}`,
+		anchor: ['top', 'left', 'right'],
+		child: myLabel,
+	})
+}
+
+App.config({ 
+	windows: [
+		Bar(0),
+		Bar(1),
+	],
 })
