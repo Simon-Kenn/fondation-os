e11fb30d79c908fa6eec976356c53260e0b5d0a7
leto <leto@fondation.org>
2024-09-05 12:14:10 +0200

    all packages additionnal called is called from hyprland itself

--- home/optionnal/desktops/hyprland/default.nix
+++ home/optionnal/desktops/hyprland/default.nix
@@ -9,8 +9,19 @@
     ./rules.nix
   ];
 
-  home.packages = [
+  home.packages = with pkgs; [
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+    grim
+    gtk3
+    imv # image viewer for x11/wayland
+    pulseaudio
+    slurp # Select a region in a wayland compositor
+    waypipe # Proxy for wayland client
+    wf-recorder # Screen recording
+    wl-clipboard # Command line copy / paste utility
+    wl-mirror
+    xdg-utils
+
   ];
 
   wayland.windowManager.hyprland = {
