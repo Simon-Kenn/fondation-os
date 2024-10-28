d92fbc42e6d345a2be06ce33e18a17d7199da80b
leto <leto@fondation.org>
2024-10-28 12:18:12 +0100

    holo

--- systems/x86_64-linux-iso/air-gapped/default.nix
+++ systems/x86_64-linux-iso/air-gapped/default.nix
@@ -1,7 +1,28 @@
 {
-  lib,
-  outputs,
-  ...
+lib,
+outputs,
+self,
+pkgs,
+...
 }: {
   imports = builtins.attrValues outputs.nixosModules;
+
+  isoImage = {
+    isoName = "yubikeyLive.iso";
+    squashfsCompression = "zstd";
+
+    appendToMenuLabel = " YubiKey Live ${self.lastModifiedDate}";
+    makeEfiBootable = true; # EFI booting
+    makeUsbBootable = true; # USB booting
+  };
+
+  swapDevices = [];
+
+  services = {
+    pcscd.enable = true;
+    udev.packages = [pkgs.yubikey-personalization];
+    getty.autologinUser = "nixos";
+
+  };
+
 }
