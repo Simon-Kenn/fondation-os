5bd1454bc5a20a9c61eab1599f660deac4fbb690
leto <leto@fondation.org>
2024-10-28 12:17:54 +0100

    update swap size

--- systems/x86_64-linux/farstar/disk.nix
+++ systems/x86_64-linux/farstar/disk.nix
@@ -15,7 +15,7 @@
               type = "EF00";
               content = {
                 type = "filesystem";
-                extraArgs = ["-n NIXBOOT"];
+                extraArgs = ["-n" "NIXBOOT"];
                 format = "vfat";
                 mountOptions = ["umask=0077"];
                 mountpoint = "/boot";
@@ -31,7 +31,7 @@
                 };
                 content = {
                   type = "btrfs";
-                  extraArgs = ["-f" "-L NIXROOT"];
+                  extraArgs = ["-f" "-L" "NIXROOT"];
                   postCreateHook =
                     /*
                     sh
@@ -57,7 +57,7 @@
                     };
                     "/swap" = {
                       mountpoint = "/.swapvol";
-                      swap.swapfile.size = "20M";
+                      swap.swapfile.size = "16G";
                       mountOptions = ["noatime"];
                     };
                   };
