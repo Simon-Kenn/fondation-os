3add420fc58bab56dc68d3af9cd5015eaf66693c
leto <leto@fondation.org>
2024-10-29 15:54:32 +0100

    laptop screen under edp-1

--- modules/home-manager/programs/wayland/hyprland/rules.nix
+++ modules/home-manager/programs/wayland/hyprland/rules.nix
@@ -1,9 +1,9 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1, 1920x1080@60, 0x0, 1"
-      "DP-1, 1920x1080@60, 1920x0, 1"
-      "HDMI-A-2, 1920x1080@60, 3840x0, 1"
+      "eDP-1, 1920x1080@60, 0x1080, 1"
+      "DP-1, 1920x1080@60, 0x0, 1"
+      "HDMI-A-2, 1920x1080@60, 1920x0, 1"
     ];
 
     windowrulev2 = [
